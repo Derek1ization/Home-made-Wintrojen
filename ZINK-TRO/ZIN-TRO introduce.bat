f4e7dbf53dc0bcd939d4bd02fa042e9335c4c1c9
Derek1ization <Derek1ization@gmail.com>
2021-03-21 09:22:01 -0500

    Add files via upload

---
+++ ZINK-TRO/ZIN-TRO introduce.bat
@@ -0,0 +1,6 @@
+@echo off
+color 0C 
+Echo                                               WARNING
+echo Hello user, How are you today. I hope you have opened this readme file befor opening anything else in this folder Im just saying User, Open this file and runing it is not recommand. I hope you read and say all the warning messages i left to warn you to not to open this home made windows trojen. TROJEN:)
+pause
+
